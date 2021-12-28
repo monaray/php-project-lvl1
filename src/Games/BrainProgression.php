421e72eec43385b75e57cbf8d8d42a7f090385c7
Lolita Naumenko <ilolita796@gmail.com>
2021-12-28 14:30:55 +0200

    add BrainProgression.php

---
+++ src/Games/BrainProgression.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Src\Games\BrainProgression;
+
+use function Src\Engine\EngineGame;
+
+const DESCRIPTION = 'What number is missing in the progression?';
+
+function Progression(): void
+{
+    $getQuestionAndAnswer = function (): array {
+        $firstNumber = random_int(1, 99);
+        $progressionStep = random_int(2, 6);
+        $randNumbers = [];
+        $randNumbers[0] = $firstNumber;
+        for ($i = 0; $i < 9; $i++) {
+            $randNumbers[$i + 1] = $randNumbers[$i] + $progressionStep;
+        }
+        $rand_key = array_rand($randNumbers, 1);
+        $correctAnswer = $randNumbers[$rand_key];
+        $randNumbers[$rand_key] = '..';
+        $question = implode(" ", $randNumbers);
+
+        return [$question, $correctAnswer];
+    };
+
+    EngineGame($getQuestionAndAnswer, DESCRIPTION);
+}
